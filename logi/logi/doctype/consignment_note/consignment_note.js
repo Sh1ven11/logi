ca124da8446c762109d3c967afc96122e3385408
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-30 15:58:55 +0530

    Stabilize Logi app: doctypes migrated and fixtures locked

---
+++ logi/logi/doctype/consignment_note/consignment_note.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Shiven Gupta and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Consignment Note", {
+// 	refresh(frm) {
+
+// 	},
+// });
